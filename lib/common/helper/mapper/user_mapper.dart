4e4631bae24822c2fc4b7907c109ff12863bf466
avislamongan@gmail.com <avislamongan@gmail.com>
2024-11-07 22:29:08 +0700

    fix: user stuff

---
+++ lib/common/helper/mapper/user_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:lecognition/domain/user/entities/user.dart';
+
+class UserMapper {
+  static UserEntity toEntity(UserEntity data) {
+    return UserEntity(
+      id: data.id,
+      username: data.username,
+      email: data.email,
+      password: data.password,
+    );
+  }
+}
--- lib/domain/user/usecases/get_user_profile.dart
+++ lib/domain/user/usecases/get_user_profile.dart
@@ -1,16 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:lecognition/core/usecase/usecase.dart';
-import 'package:lecognition/data/auth/models/signup_req_params.dart';
 // import 'package:lecognition/domain/auth/repositories/auth.dart';
 import 'package:lecognition/domain/user/repositories/user.dart';
 import 'package:lecognition/service_locator.dart';
 
-class GetUserProfileUseCase extends Usecase<Either,SignupReqParams> {
+class GetUserProfileUseCase extends Usecase<Either, dynamic> {
   // SignupUseCase({required this.authRepository});
   // final AuthRepository authRepository;
-  
+
   @override
-  Future<Either> call({SignupReqParams? params}) async {
-    return await sl<UserRepository>().getUserProfile(params!);
+  Future<Either> call({params}) async {
+    return await sl<UserRepository>().getUserProfile();
   }
 }
