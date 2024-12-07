a924385fbbf9d6b12b9de92656af72c5709e9520
Lukman_28 <lukmaneka216@gmail.com>
2024-12-07 16:22:51 +0700

    feat: update color theme

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-    static const primary = Color.fromARGB(255, 52, 121, 40); // Set warna primary secara eksplisit
-    static const secondary = Color.fromARGB(255, 192, 235, 166); // Set warna secondary secara eksplisit
-    static const onPrimary = Color.fromARGB(255, 255, 251, 230); // Set warna teks pada primary secara eksplisit
-    static const onSecondary = Color.fromARGB(255, 255, 255, 255); 
+    static const primary = Color.fromARGB(255, 255, 131, 23);
+    static const secondary = Color.fromARGB(255, 243, 198, 35);
+    static const onPrimary = Color.fromARGB(255, 244, 246, 255);
+    static const onSecondary = Color.fromARGB(255, 16, 55, 92);
 }
